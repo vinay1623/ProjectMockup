19b38070c49e81a9817317637189761dbc5683e4
vinay1623 <vinaykushwaha108@gmail.com>
2021-10-16 10:35:00 +0530

    data

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from "./MOCK_DATA.json"
+import Views from './Views';
+
+
+
+
+
+
+function App() {
+
+  
+function hello()
+{
+  
+  alert("user display profile not working");
+}
+
+  return (
+    <div className="App">
+      <div >
+        <table className="table">
+          <tr>
+            <th>ID</th>
+            <th>Avatar</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>IP Address</th>
+            <th>Profile View</th>
+          </tr>
+      {
+        Data.map(post =>{
+          return(
+            
+              <tr>
+            <td>{post.id}</td>
+            <td><img src={post.avatar} alt="image" /></td>
+            <td>{post.first_name}</td>
+            <td>{post.last_name}</td>
+            <td>{post.email}</td>
+            <td>{post.gender}</td>
+            <td>{post.ip_address}</td>
+            <td><button name="view" onClick={hello}>view</button></td>
+            
+            </tr>
+            
+             
+
+
+            
+
+
+          )
+        })
+      }
+      </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
